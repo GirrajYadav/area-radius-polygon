59018220001b86551a87ba1e0b59ddec7e2bcbce
GirrajYadav <54210555+GirrajYadav@users.noreply.github.com>
2019-12-25 22:28:30 +0530

    Add files via upload

---
+++ polygon.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <math.h>
+int main() {
+   float r = 4;
+   float a = 12;
+   float area = ((r * r * a) * sin((360 / n) * 3.14159 / 180)) / 2;
+   std::cout<<"area="<<area;
+   return 0;
+}
